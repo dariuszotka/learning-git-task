b41d96b8283383ae6a59aa8c087397fc37698ff5
Dorota Pietruszka <do.pietruszka@gmail.com>
2022-03-17 13:30:14 +0100

    iter and capitalize shopping-list1.py

--- shopping-list1.py
+++ shopping-list1.py
@@ -2,3 +2,6 @@ dict = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"] 
 }
+for keys, values in dict.items():
+    print(f"Idę do {keys.capitalize()}")
+    print(f"Kupuję tam {[v.capitalize() for v in values]}")
