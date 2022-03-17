2606802427d7c1c40c7543be3da5f4c37d8a5656
Dorota Pietruszka <do.pietruszka@gmail.com>
2022-03-17 13:32:22 +0100

    total product count shopping-list1.py

--- shopping-list1.py
+++ shopping-list1.py
@@ -5,3 +5,15 @@ dict = {
 for keys, values in dict.items():
     print(f"Idę do {keys.capitalize()}")
     print(f"Kupuję tam {[v.capitalize() for v in values]}")
+total = 0
+
+for i in dict:
+    value = dict[i]
+    count = len(value)
+    total += count
+  
+print(f"W sumie kupuję {total} rzeczy")
+
+#albo
+
+print(sum(map(len, dict.values())))
